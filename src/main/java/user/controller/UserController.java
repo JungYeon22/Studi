cd5e9ce101fa9249803405433831f18c0df638b3
chlry <chlrytns94@naver.com>
2024-04-05 11:15:13 +0900

    Feat: 로그인, 회원가입 기능 구현

--- src/main/java/user/controller/UserController.java
+++ src/main/java/user/controller/UserController.java
@@ -1,35 +1,66 @@
 package user.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import user.bean.UserDTO;
+import user.service.UserService;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+import java.util.Map;
 
 @Controller
 @RequestMapping(value = "/user")
+@RequiredArgsConstructor
 public class UserController {
-    @GetMapping(value = "/loginForm")
-    public String loginForm(){
-        return "user/loginForm";
-    }
 
-    @GetMapping(value = "/writeForm")
-    public String writeForm(){
+    private final UserService userService;
+
+    @GetMapping("/writeForm")
+    public String writeForm() {
         return "user/writeForm";
     }
 
-
-    @GetMapping(value = "/introduceForm")
-    public String introduceForm() {
-        return "user/introduceForm";
+    @PostMapping("/writeForm")
+    public String save(@ModelAttribute UserDTO userDTO) {
+        int saveResult = userService.save(userDTO);
+        if (saveResult > 0) {
+            return "user/loginForm";
+        } else {
+            return "user/writeForm";
+        }
     }
 
-    @GetMapping(value = "/myPage")
-    public String myPage(){
-        return "user/myPage";
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "user/loginForm";
     }
 
-    @GetMapping(value = "/managerPage")
-    public String managerPage(){
-        return "user/managerPage";
+    @PostMapping("/loginForm")
+    public String login(@ModelAttribute UserDTO userDTO,
+                        HttpSession session) {
+        UserDTO loginResult = userService.login(userDTO);
+        if (loginResult != null) {
+            session.setAttribute("userDTO", loginResult);
+            return "redirect:/";
+        } else {
+            return "user/loginForm";
+        }
     }
+
+    @PostMapping("/checkUserId")
+    @ResponseBody
+    public ResponseEntity<Map<String, Boolean>> checkUserId(@RequestBody Map<String, String> json) {
+        boolean isDuplicate;
+        String userId = json.get("userId");
+        if (userId.isEmpty()) {
+            isDuplicate = true;
+        } else {
+            isDuplicate = userService.checkUserId(userId);
+        }
+        return ResponseEntity.ok(Collections.singletonMap("isDuplicate", isDuplicate));
+    }
+
 }
