ee3821a1019333013e31b436beebe67bc8fc58f5
kimyonghwan1996 <137673370+kimyonghwan1996@users.noreply.github.com>
2024-04-09 15:03:21 +0900

    Feat : managerpage 대시보드 및 유저리스트 추가

--- src/main/java/manager/service/ManagerService.java
+++ src/main/java/manager/service/ManagerService.java
@@ -1,10 +1,13 @@
 package manager.service;
 
+
+
 import java.util.List;
 import java.util.Map;
-public interface ManagerService {
 
-    public int getToTalMember(String hiredate);
-    public int getWeekMember(String hiredate);
-    public List<Map<String, Object>> getTodayMember(String hiredate);
+public interface ManagerService {
+    public List<Map<String, Object>> getTodayMember();
+    public List<Map<String, Object>> boardUpload();
+
+    public Map<String, Object> getUserList(String pg);
 }
