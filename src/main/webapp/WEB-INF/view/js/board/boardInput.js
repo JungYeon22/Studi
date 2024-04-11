972385c1a72c13da2272e0586dc25cc10356725d
roarquokkaking <ldw9581@naver.com>
2024-04-11 18:02:17 +0900

    <feat>: boardInput 유효성검사 완료

--- src/main/webapp/WEB-INF/view/js/board/boardInput.js
+++ src/main/webapp/WEB-INF/view/js/board/boardInput.js
@@ -2,20 +2,37 @@ $(function(){
 
     $('#boardInputFormBtn').click(function(){
 
-        $.ajax({
-            type:'post',
-            url:'boardInputData',
-            data:$('#boardInputForm').serialize(),
-            success:function(data){
-                alert('글쓰기 완료');
-                location.href='boardListMain';
+        $('#subjectDiv').empty();
+        $('#userTotCntDiv').empty();
 
-            },
-            error:function(e){
-                console.log(e);
-            }
 
-        });
+        if($('#subject').val()==''){
+            $('#subjectDiv').text('제목을 입력하세요');
+
+        }else if($('#userTotCnt').val()==''){
+            $('#userTotCntDiv').html('<font color="red">모집인원을 입력해주세요.</font>');
+        }else if($('#projectType').val()=='default'){
+            $('#userTotCntDiv').html('<font color="blue">모집유형을 선택해주세요.</font>');
+        }else if($('#projectField').val()=='default'){
+            $('#userTotCntDiv').html('<font color="green">모집분야을 선택해주세요.</font>');
+        }else if(!$('input:checked[name="lang"]').is(":checked")){
+            $('#langDiv').text('언어를 선택해주세요');
+        }else  {
+            $.ajax({
+                type: 'post',
+                url: 'boardInputData',
+                data: $('#boardInputForm').serialize(),
+                success: function (data) {
+                    alert('글쓰기 완료');
+                    location.href = 'boardListMain';
+
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+
+            });
+        }
     });
 
 });
